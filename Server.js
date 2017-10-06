79f0e8722ff5d68959c2471939394278dc8caa14
Quinton Swanepoel <u15245510@tuks.co.za>
2017-10-06 10:00:14 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,45 +5,14 @@ var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-function sendMessageToSlackResponseURL(responseURL, JSONmessage){
-    var postOptions = {
-        url: responseURL,
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        json: JSONmessage
-    }
-request(postOptions, (error, response, body) => {
-        if (error){
-            // handle errors as you see fit
-        }
-    })
-}
-
 app.post('/', urlencodedParser, (req, res) =>{
         res.status(200).end();
-        //var actionJSONPayload = JSON.parse(req.body.payload);
+        console.log(req.body);
         var reqBody = req.body;
-        var responseURL = reqBody.response_url;
-        console.log(reqBody);
-        if(reqBody.token != 'CVuvCq700N50MgszRrYNn5x7')
-        {
-            res.status(403).end("Access forbidden");
-        }else{
-                var googleScript = {
-                    "name": "GetHours",
-                    "response_url": reqBody.response_url,
-                    "user": reqBody.user_name
-                }
-                sendMessageToSlackResponseURL("https://script.google.com/macros/s/AKfycbyoQBvG09Pa8AZiDDEKNtgsPtBmJK7lma-QC7CjeKyKfrA42pJG/exec", googleScript); 
-             
-            
-                    
-        }
+        res.send("GOOD");
     
 });
 
-var port = 9002;
+var port = 80;
 app.listen(port);
 console.log('Listening on port',port);
